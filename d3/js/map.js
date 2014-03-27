1c2f9eca627c2140d295000323d41a5f4731a662
maxime <maxime@jesus>
2014-03-27 14:23:00 +0100

    fix some bugs add some comments

--- d3/js/map.js
+++ d3/js/map.js
@@ -1,97 +1,66 @@
-var continents, group, height, path, patternNegatif, patternPositif, svg, topMargin, width;
 
-path = d3.geo.path().projection(d3.geo.eckert3().scale(180));
 
-width = window.innerWidth - 150;
+//Some variables use for the map's position
+var width = 960,
+    height = 500,
+    topMargin = 50;
 
-topMargin = 50;
+//They are many projections possibilities as you can see from here: https://github.com/d3/d3-geo-projection/
+var projection = d3.geo.eckert3().scale(180);
+var path = d3.geo.path().projection(projection);
 
-height = window.innerHeight;
+//We add the svg to the html
+var svg = d3.select("#container").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class","worldmap");
 
-svg = d3.select("#worldmap").attr("width", width).attr("height", height);
+//We add a group where we'll put our continents and countries in
+var group = svg.append("g")
+    .attr("width", "100%")
+    .attr("height", "100%")
 
-patternPositif = svg.append("svg:defs").append("svg:pattern").attr("id", "dotsPatternPositif").attr("x", 0).attr("y", 0).attr("width", 8).attr("height", 8).attr("patternUnits", "userSpaceOnUse").attr("patternContentUnits", "userSpaceOnUse");
+//You
+function getClassFromNode(d) {
+    if (d.id > 5) {
+        return " negatif";
+    }
+    return " positif";
+}
 
-patternPositif.append("svg:circle").attr("r", 3).attr("cx", 4).attr("cy", 4).style("fill", "#00A700");
+//We have modified the original continent-geogame-110m.json file to make zooms more easier, so some countries won't show up with all theirs parts
+// like French Guiana for France.
+d3.json("data/continent-geogame-110m-countrieszoom.json", function(error, world) {
+    // See here to get more info about the following line: https://github.com/mbostock/topojson
+    var countries = topojson.feature(world, world.objects.countries);
+    //We group the countries in contients (We removed Antartica and we put ocenania with Asia)
+    var asia = {type: "FeatureCollection", name: "Asia", id:1, features: countries.features.filter(function(d) { return d.properties.continent=="Asia"; })};
+    var africa = {type: "FeatureCollection", name: "Africa", id:2, features: countries.features.filter(function(d) { return d.properties.continent=="Africa"; })};
+    var europe = {type: "FeatureCollection", name: "Europe", id:3, features: countries.features.filter(function(d) { return d.properties.continent=="Europe"; })};
+    var na = {type: "FeatureCollection", name: "North America", id:4, features: countries.features.filter(function(d) { return d.properties.continent=="North America"; })};
+    var sa = {type: "FeatureCollection", name: "South America", id:5, features: countries.features.filter(function(d) { return d.properties.continent=="South America"; })};
 
-patternNegatif = svg.append("svg:defs").append("svg:pattern").attr("id", "dotsPatternNegatif").attr("x", 0).attr("y", 0).attr("width", 8).attr("height", 8).attr("patternUnits", "userSpaceOnUse");
-
-patternNegatif.append("svg:circle").attr("r", 3).attr("cx", 4).attr("cy", 4).style("fill", "#cd2514");
-
-group = svg.append("g").attr("width", "100%").attr("height", "100%");
-
-continents = void 0;
-
-d3.json("data/continent-geogame-110m.json", function(error, world) {
-    console.log('ok');
-    var africa, asia, continent, continentBBox, countries, europe, focus, isFocusedOnContinent, isFocusedOnCountry, na, previousTransformation, sa, scaleFactor, worldmapBBox, worldmapBBoxOffsetX, worldmapBBoxOffsetY, _i, _len;
-    countries = topojson.feature(world, world.objects.countries).features;
-    asia = {
-        type: "FeatureCollection",
-        name: "Asia",
-        id: 1,
-        features: countries.filter(function(d) {
-            return d.properties.continent === "Asia";
-        })
-    };
-    africa = {
-        type: "FeatureCollection",
-        name: "Africa",
-        id: 2,
-        features: countries.filter(function(d) {
-            return d.properties.continent === "Africa";
-        })
-    };
-    europe = {
-        type: "FeatureCollection",
-        name: "Europe",
-        id: 3,
-        features: countries.filter(function(d) {
-            return d.properties.continent === "Europe";
-        })
-    };
-    na = {
-        type: "FeatureCollection",
-        name: "North America",
-        id: 4,
-        features: countries.filter(function(d) {
-            return d.properties.continent === "North America";
-        })
-    };
-    sa = {
-        type: "FeatureCollection",
-        name: "South America",
-        id: 5,
-        features: countries.filter(function(d) {
-            return d.properties.continent === "South America";
-        })
-    };
-    continents = [asia, africa, europe, na, sa];
+    var continents = [asia,africa,europe,na,sa];
+    var focus, isFocusedOnContinent, isFocusedOnCountry, previousTransformation, scaleFactor, worldmapBBox, worldmapBBoxOffsetX, worldmapBBoxOffsetY,selectedCountrycontinent,translateX, translateY;;
+    //There is 3 levels of zoom = "world, continent and country"
     isFocusedOnContinent = false;
     isFocusedOnCountry = false;
-    previousTransformation = null;
-    scaleFactor = null;
-    worldmapBBox = null;
-    worldmapBBoxOffsetX = null;
-    worldmapBBoxOffsetY = null;
+
     focus = function(clickedCountry) {
-        var continent, translateX, translateY;
-        if ((clickedCountry == null) && !isFocusedOnCountry) {
+
+        //zoom continent => zoom world
+        if ((clickedCountry == null) && !isFocusedOnCountry && isFocusedOnContinent) {
             isFocusedOnContinent = false;
-            return group.selectAll(".continent").transition().duration(200).attr("transform", "").each("end", function() {
-                return group.selectAll(".continent").attr("class", function(d) {
-                    var classColor;
-                    if (d.id % 2 === 0) {
-                        classColor = " negatif";
-                    } else {
-                        classColor = " positif";
-                    }
-                    return 'continent' + classColor;
+            group.selectAll(".continent").transition().duration(200).attr("transform", "").each("end", function() {
+                group.selectAll(".continent").attr("class", function(d) {
+                    return 'continent' + getClassFromNode(d);
                 });
             });
         } else if (isFocusedOnContinent) {
+            //Zoom continent => zoom country
             if (!isFocusedOnCountry) {
                 isFocusedOnCountry = true;
+                selectedCountry = clickedCountry;
                 group.selectAll(".country").filter(function(d) {
                     if (d !== clickedCountry) {
                         return d;
@@ -104,13 +73,7 @@ d3.json("data/continent-geogame-110m.json", function(error, world) {
                         return d;
                     }
                 }).attr("class", function(d) {
-                    var classColor;
-                    if (d.id % 2 === 0) {
-                        classColor = " negatif";
-                    } else {
-                        classColor = " positif";
-                    }
-                    return 'country focused' + classColor;
+                    return 'country focused' + getClassFromNode(d);
                 }).transition().duration(600).attr("transform", function() {
                     var bbox, miniCountryOffsetX, miniCountryOffsetY, miniCountryScale, targetSize;
                     bbox = this.getBBox();
@@ -121,43 +84,24 @@ d3.json("data/continent-geogame-110m.json", function(error, world) {
                     return "translate(" + miniCountryOffsetX + "," + miniCountryOffsetY + ") scale(" + miniCountryScale + ")";
                 }).each("end", function() {
                     document.getElementById("country-details").className = "";
-                    $scope.$parent.country = clickedCountry.properties.name;
-                    $scope.$parent.svgMan = "images/man.svg";
-                    $scope.$parent.occupationRate = Math.round(100 * Math.random(), 1);
-                    $timeout(function() {
-                        d3.select('#man .fill-level').style("fill", dbUtils.colorScale(1 - ($scope.$parent.occupationRate + 10) / 100)).transition().duration(800).attr("y", 11 * (100 - $scope.$parent.occupationRate)).attr("height", 11 * $scope.$parent.occupationRate);
-                        return d3.select(".indicator").style("color", dbUtils.colorScale(1 - ($scope.$parent.occupationRate + 10) / 100)).select(".value").transition().duration(800).tween("text", function() {
-                            var i;
-                            i = d3.interpolate(this.textContent, $scope.$parent.occupationRate);
-                            return function(t) {
-                                return this.textContent = Math.round(i(t));
-                            };
-                        });
-                    }, 400);
-                    if (!$scope.$$phase) {
-                        return $scope.$apply();
-                    }
+                    document.getElementById("country-name").textContent = clickedCountry.properties.name;
                 });
-            } else if (clickedCountry != null) {
+            } else {
+                //Zoom country => zoom continent
                 document.getElementById("country-details").className = "faded";
                 isFocusedOnCountry = false;
                 group.selectAll(".country").attr("class", function(d) {
-                    var classColor;
-                    if (d.id % 2 === 0) {
-                        classColor = " negatif";
-                    } else {
-                        classColor = " positif";
-                    }
-                    return 'country' + classColor;
+                    return 'country' + getClassFromNode(d);
                 });
                 group.selectAll(".country").filter(function(d) {
-                    if (d === clickedCountry) {
+                    if (d === selectedCountry) {
                         return d;
                     }
                 }).transition().attr("transform", "");
-                return group.select(".continent.focused").transition().delay(50).duration(200).attr("transform", previousTransformation);
+                group.select(".continent.focused").transition().delay(50).duration(200).attr("transform", previousTransformation);
             }
-        } else {
+        //Zoom world => zoom continent
+        } else if(!(clickedCountry == null)) {
             translateX = null;
             translateY = null;
             isFocusedOnContinent = true;
@@ -169,39 +113,31 @@ d3.json("data/continent-geogame-110m.json", function(error, world) {
             }).attr("class", function() {
                 return this.className.animVal + " unfocused";
             });
-            return group.selectAll(".continent").filter(function(d) {
+            group.selectAll(".continent").filter(function(d) {
                 if (d.name === continent) {
                     return d;
                 }
             }).attr("class", "continent focused").transition().duration(400).attr("transform", function() {
-                var bbox;
+                var bbox, scaleFactor;
                 bbox = this.getBBox();
                 scaleFactor = Math.min(width * 0.7 / bbox.width, height * 0.7 / (bbox.height + topMargin));
                 translateX = worldmapBBox.width / 2 - scaleFactor * (bbox.x + bbox.width / 2);
                 translateY = worldmapBBox.height / 2 - scaleFactor * (bbox.y + bbox.height / 2);
                 return "translate(" + translateX + "," + translateY + ") scale(" + scaleFactor + ")";
             });
+            group.selectAll(".country").attr("class", function(d) {
+                return 'country' + getClassFromNode(d);
+            });
         }
     };
+
     group.selectAll(".continent").data(continents).enter().call(function() {
         return this.append("g").attr('class', function(d) {
-            var classColor;
-            if (d.id % 2 === 0) {
-                classColor = " negatif";
-            } else {
-                classColor = " positif";
-            }
-            return 'continent ' + d.name.replace(' ', '') + classColor;
+            return 'continent ' + d.name.replace(' ', '') + getClassFromNode(d);
         }).selectAll(".country").data(function(d) {
             return d.features;
         }).enter().insert("path").attr("class", function(d) {
-            var classColor;
-            if (d.id % 2 === 0) {
-                classColor = " negatif";
-            } else {
-                classColor = " positif";
-            }
-            return "country" + classColor;
+            return "country" + getClassFromNode(d);
         }).attr("d", path).attr("id", function(d) {
             return d.id;
         }).attr("title", function(d) {
@@ -211,6 +147,8 @@ d3.json("data/continent-geogame-110m.json", function(error, world) {
             d3.event.stopPropagation();
         });
     });
+    var continent, continentBBox, _i, _len;
+
     for (_i = 0, _len = continents.length; _i < _len; _i++) {
         continent = continents[_i];
         continentBBox = null;
@@ -228,11 +166,13 @@ d3.json("data/continent-geogame-110m.json", function(error, world) {
             return "translate(" + textOffsetX + "," + textOffsetY + ")";
         });
     }
+
     d3.select("body").on("click", function() {
         focus();
         d3.event.stopPropagation();
     });
-    return group.attr("transform", function() {
+
+    group.attr("transform", function() {
         var mapScaleFactor;
         worldmapBBox = this.getBBox();
         mapScaleFactor = 1;
